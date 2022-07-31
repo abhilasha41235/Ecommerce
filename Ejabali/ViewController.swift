528794d47431c35d42c4256a70b1cfeabc77e5bf
Thapliyal, Abhilasha <abhilasha_thapliyal@optum.com>
2022-07-31 22:50:44 +0530

    Initial Commit

---
+++ Ejabali/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Ejabali
+//
+//  Created by Thapliyal, Abhilasha on 31/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
